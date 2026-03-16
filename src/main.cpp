f19e7a3bd93e4424f55cfad9816d3d0f88d1bfd0
OutragedFinn <61402835+OutragedFinn@users.noreply.github.com>
2026-03-16 21:35:41 +0800

    setup

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <stdlib.h>
+
+//Alignment Sensors
+#define IR_FLA A0
+#define IR_BLA A1
+#define IR_FRA A2
+#define IR_BRA A3
+
+#define IR_PIPE A4
+#define IR_PLATF A5
+
+#define IR_FLCOL A8
+#define IR_BLCOL A9
+#define IR_FRCOL A10
+#define IR_BRCOL A11
+
+#define MOT_ENC1 2
+#define MOT_ENC2 3
+#define MOT_ENC3 4
+#define MOT_ENC4 5
+
+#define COL_SERVO1 6
+#define COL_SERVO2
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
