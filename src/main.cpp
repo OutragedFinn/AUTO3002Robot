c2d497547ff7d6b6571ba49f707d093dcb2e7054
OutragedFinn <61402835+OutragedFinn@users.noreply.github.com>
2026-03-27 11:34:03 +0800

    Control Loop and drive functions for COLLECT

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <stdlib.h>
+#include <Servo.h>
 
 #define IR_FL_A A0
 #define IR_BL_A A1
@@ -14,10 +15,8 @@
 #define IR_FR_COL A10
 #define IR_BR_COL A11
 
-#define MOT_FL_EN 2
-#define MOT_BL_EN 3
-#define MOT_FR_EN 4
-#define MOT_BR_EN 5
+#define MOT_L_EN 2
+#define MOT_R_EN 3
 
 #define COL_SERVO1 6
 #define COL_SERVO2 7
@@ -27,14 +26,10 @@
 #define ACT_EN1 10
 #define ACT_EN2 11
 
-#define MOT_FL_FWD 22
-#define MOT_FL_REV 23
-// #define MOT_BL_FWD 24
-// #define MOT_BL_REV 25
-#define MOT_FR_FWD 24
-#define MOT_FR_REV 25
-// #define MOT_BR_FWD 28
-// #define MOT_BR_REV 29
+#define MOT_L_FWD 22
+#define MOT_L_REV 23
+#define MOT_R_FWD 24
+#define MOT_R_REV 25
 
 #define LIFT_UP 30
 #define LIFT_DOWN 31
@@ -42,28 +37,26 @@
 #define ROT_IN1 32
 #define ROT_IN2 33
 
-#define MOT_FL_ENCA 34
-// #define MOT_BL_ENCA 36
-#define MOT_FR_ENCA 35
-// #define MOT_BR_ENCA 40
+#define MOT_L_ENCA 21
+#define MOT_R_ENCA 35
+#define MOT_L_ENCB 36
+#define MOT_R_ENCB 37
 
-#define MOT_FL_ENCB 36
-// #define MOT_BL_ENCB 37
-#define MOT_FR_ENCB 37
-// #define MOT_BR_ENCB 41
+#define ACT1_IN1 38
+#define ACT1_IN2 39
+#define ACT2_IN1 40
+#define ACT2_IN2 41
 
-// #define ACT1_IN1 38
-// #define ACT1_IN2 39
-// #define ACT2_IN1 40
-// #define ACT2_IN2 41
+#define SERVO_1 1000;
+#define SERVO_2 1001;
 
 // #define ROT_IN1 48
 // #define ROT_IN2 49
 
-// #define ACT1_ENC 50
-// #define ACT2_ENC 51
-// #define LIFT_ENC 52
-// #define ROT_ENC 53
+#define ACT1_ENCA 50
+#define ACT1_ENCB 51
+#define LIFT_ENC 52
+#define ROT_ENC 53
 
 int motor_L_ticks;
 int motor_R_ticks;
@@ -81,14 +74,35 @@ enum State {
     RETURN,
     END
 };
+State sequence[] = {START, COLLECT, MOVE, LIFT, DROP, RETRACT, RETURN, END};
+volatile int stateIndex = 0;
+
+enum class CollectState {
+    ALIGN,
+    ACTUATOR_EXTEND,
+    GRAB,
+    ACTUATOR_RETRACT,
+    COLLECT_DONE
+};
+
+CollectState collectSequence[] = {CollectState::ALIGN, CollectState::ACTUATOR_EXTEND, CollectState::GRAB, CollectState::ACTUATOR_RETRACT, CollectState::COLLECT_DONE};
+volatile int collectIndex = 0;
+
+enum class MoveState {
+    MOVE_FORWARD,
+    ALIGN_WALL
+};
+
+enum class DropState {
+    ALIGN,
+    ROTATE,
+};
+
+
 
 State currentState;
+CollectState currentCollect;
 
-void drive_motors() {
-
-    if
-
-}
 
 void set_drive(int L_power, int R_power) {
     motor_L_power = L_power;
@@ -97,149 +111,283 @@ void set_drive(int L_power, int R_power) {
 
 //fl, fr, bl, br
 int mot_ticks[2] = {0,0};
+int act_ticks[2] = {0,0};
 
-int read_colour_sensors() {
-    return 0;
-}
+// int read_colour_sensors() {
+//     return 0;
+// }
 
-int check_colour_sensors() {
-    int state = read_colour_sensors();
-    return 1;
-}
+// int check_colour_sensors() {
+//     int state = read_colour_sensors();
+//     return 1;
+// }
 
-void handle_fl_mot_ticks() {
-    if (digitalRead(MOT_FL_ENCB)) {
-        mot_ticks[0]++;
+volatile bool last_L_A = 0;
+void handle_l_mot_ticks() {
+    bool A = digitalRead(MOT_L_ENCA);
+    bool B = digitalRead(MOT_L_ENCB);
 
-    } else {
-        mot_ticks[0]--;
-
+    if (A != last_L_A) {          
+        if (A == B) mot_ticks[0]++; 
+        else mot_ticks[0]--;
     }
+    last_L_A = A;
 }
 
-void handle_fr_mot_ticks() {
-    if (digitalRead(MOT_FR_ENCB)) {
-        mot_ticks[1]++;
-    } else {
-        mot_ticks[1]--;
-    }  
+volatile bool last_R_A = 0;
+void handle_r_mot_ticks() {
+    bool A = digitalRead(MOT_R_ENCA);
+    bool B = digitalRead(MOT_R_ENCB);
+
+    if (A != last_R_A) {          
+        if (A == B) mot_ticks[0]++; 
+        else mot_ticks[0]--;
+    }
+    last_R_A = A;
 }
-// void handle_bl_mot_ticks() {
-//     if (digitalRead(MOT_BL_ENCB)) {
-//         mot_ticks[2]++;
-//     } else {
-//         mot_ticks[2]--;
-//     }  
-// }
-// void handle_br_mot_ticks() {
-//     if (digitalRead(MOT_BR_ENCB)) {
-//         mot_ticks[3]++;
-//     } else {
-//         mot_ticks[3]--;
-//     }  
-// }
 
+volatile bool last_act1_A = 0;
+void handle_act1_ticks() {
+    bool A = digitalRead(ACT1_ENCA);
+    bool B = digitalRead(ACT1_ENCB);
 
-volatile int enc_fl_old;
-volatile int enc_fl_new;
-volatile int enc_fr_old;
-volatile int enc_fr_new;
-volatile int speed_error = 0;
-volatile int integral = 0;
-volatile int derivative = 0;
-volatile int prev_speed_error = 0;
-volatile int correction;
-volatile int delta_enc_fl;
-volatile int delta_enc_fr;
-volatile int v_fl_actual;
-volatile int v_fr_actual;
-volatile int left_speed;
-volatile int right_speed;
-//percentage
-int base_speed = 70;
+    if (A != last_act1_A) {          
+    if (A == B) act_ticks[0]++; 
+        else act_ticks[0]--;
+    }
+    last_act1_A = A;
 
-int Kp = 3;
-int Kd = 3;
-int Ki = 3;
+}
+
+volatile int enc_l_old;
+volatile int enc_l_new;
+volatile int enc_r_old;
+volatile int enc_r_new;
+
+volatile int delta_enc_l;
+volatile int delta_enc_r;
+
+volatile float error_l = 0;
+volatile float error_r = 0;
+
+volatile float prev_error_l = 0;
+volatile float prev_error_r = 0;
+
+volatile float integral_l = 0;
+volatile float integral_r = 0;
+
+volatile float derivative_l = 0;
+volatile float derivative_r = 0;
+
+volatile float v_l_actual;
+volatile float v_r_actual;
+
+volatile float v_l_des = 600;
+volatile float v_r_des = 600;
+
+volatile float output_l;
+volatile float output_r;
+float dt = 0.02;
+
+float Kp = 0.5;
+float Kd = 0;
+float Ki = 0.1;
+float I_MAX = 2550;
 
 ISR(TIMER1_COMPA_vect){
 
-    enc_fl_new = mot_ticks[0];
-    delta_enc_fl = enc_fl_new - enc_fl_old;
-    //ticks per second
-    int v_fl_actual = delta_enc_fl*10;
+    enc_l_new = mot_ticks[0];
+    delta_enc_l = enc_l_new - enc_l_old;
+    v_l_actual = delta_enc_l / dt;
+    enc_l_old = enc_l_new;
 
-    enc_fl_old = enc_fl_new;
+    error_l = v_l_des - v_l_actual;
 
-    enc_fr_new = mot_ticks[0];
-    delta_enc_fr = enc_fr_new - enc_fr_old;
-    //ticks per second
-    int v_fr_actual = delta_enc_fr*10;
+    integral_l += error_l*dt;
+    derivative_l = (error_l - prev_error_l)/dt;
 
-    enc_fl_old = enc_fl_new;
-
-    speed_error = v_fl_actual - v_fr_actual;
-    integral+=speed_error;
-    derivative = speed_error - prev_speed_error;
+    if (integral_l > I_MAX) integral_l = I_MAX;
+    if (integral_l < -I_MAX) integral_l = -I_MAX;
+    output_l = Kp* error_l + Ki * integral_l + Kd * derivative_l;
     
-    correction = constrain(Kp*speed_error + Ki*integral + Kd*derivative,-30,30);
-    left_speed  = base_speed - correction;
-    right_speed = base_speed + correction;
-    set_drive(left_speed, right_speed)
+    prev_error_l = error_l;
+
+    if (output_l > 255) output_l = 255;
+    if (output_l < -255) output_l = -255;
+
+    enc_r_new = mot_ticks[1];
+    delta_enc_r = enc_r_new - enc_r_old;
+    v_r_actual = delta_enc_r / dt;
+    enc_r_old = enc_r_new;
+
+    error_r = v_r_des - v_r_actual;
+
+    integral_r += error_r*dt;
+    derivative_r = (error_r - prev_error_r)/dt;
+
+    if (integral_r > I_MAX) integral_r = I_MAX;
+    if (integral_r < -I_MAX) integral_r = -I_MAX;
+    output_r = Kp* error_r + Ki * integral_r + Kd * derivative_r;
+    
+    prev_error_r = error_r;
+
+    if (output_r > 255) output_r = 255;
+    if (output_r < -255) output_r = -255;
+    
+
+    // enc_fr_new = mot_ticks[0];
+    // delta_enc_fr = enc_fr_new - enc_fr_old;
+    // //ticks per second
+    // int v_fr_actual = delta_enc_fr*10;
+
+    // enc_fl_old = enc_fl_new;
+
+    // speed_error = v_fl_actual - v_fr_actual;
+    // integral+=speed_error;
+    // derivative = speed_error - prev_speed_error;
+    
+    // correction = constrain(Kp*speed_error + Ki*integral + Kd*derivative,-30,30);
+    // left_speed  = base_speed - correction;
+    // right_speed = base_speed + correction;
+    // set_drive(left_speed, right_speed);
 
 
+};
+
+//drive may be dependant on sensors
+void drive_motors() {
+
+    if (currentState == MOVE) {
+        analogWrite(MOT_L_EN, int(output_l));
+        analogWrite(MOT_R_EN, int(output_r));
+
+        digitalWrite(MOT_L_FWD, HIGH);
+        digitalWrite(MOT_L_REV, LOW);
+
+        digitalWrite(MOT_R_FWD, HIGH);
+        digitalWrite(MOT_R_REV, LOW);
+    }
+
+    if (currentState == RETURN) {
+        analogWrite(MOT_L_EN, int(output_l));
+        analogWrite(MOT_R_EN, int(output_r));
+
+        digitalWrite(MOT_L_FWD, LOW);
+        digitalWrite(MOT_L_REV, HIGH);
+
+        digitalWrite(MOT_R_FWD, LOW);
+        digitalWrite(MOT_R_REV, HIGH);
+    }
+}
+
+
+volatile int actuatorTicks;
+
+
+void drive_actuators() {
+    if (currentCollect == CollectState::ACTUATOR_EXTEND) {
+        if (actuatorTicks >= 100) {
+            nextCollect();
+        } else {
+            analogWrite(ACT_EN1, 255);
+            digitalWrite(ACT1_IN1,HIGH);
+            digitalWrite(ACT1_IN2,LOW);
+        } 
+    }
+    if (currentCollect == CollectState::ACTUATOR_RETRACT) {
+        if (actuatorTicks <= 0) {
+            nextCollect();
+        } else {
+            analogWrite(ACT_EN1, 255);
+            digitalWrite(ACT1_IN1,LOW);
+            digitalWrite(ACT1_IN2,HIGH);
+        } 
+    }
+}
+
+Servo collectorServo1;
+Servo collectorServo2;
+
+void drive_servo() {
+    collectorServo1.write(180);
+    collectorServo2.write(180);
+}
+
+void nextState() {
+    stateIndex++;
+    currentState = sequence[stateIndex];
+
+}
+
+void nextCollect() {
+    collectIndex++;
+    currentCollect = collectSequence[collectIndex];
 }
 
 void setup() {
-    currentState = START;
+    // currentState = START;
+    Serial.begin(9600); 
 
-    //Motors
-    pinMode(MOT_BL_EN, OUTPUT);
-    pinMode(MOT_BR_EN, OUTPUT);
-    pinMode(MOT_FL_EN, OUTPUT);
-    pinMode(MOT_FL_EN, OUTPUT);
+    // Motor control
+    pinMode(MOT_L_EN, OUTPUT);
+    pinMode(MOT_R_EN, OUTPUT);
 
-    // pinMode(MOT_BL_FWD, OUTPUT);
-    // pinMode(MOT_BL_REV, OUTPUT);
-    pinMode(MOT_FL_FWD, OUTPUT);
-    pinMode(MOT_FL_REV, OUTPUT);
-    // pinMode(MOT_BR_FWD, OUTPUT);
-    // pinMode(MOT_BR_REV, OUTPUT);
-    pinMode(MOT_FR_FWD, OUTPUT);
-    pinMode(MOT_FR_REV, OUTPUT);
-    // pinMode(MOT_BR_FWD, OUTPUT);
-    // pinMode(MOT_BR_REV, OUTPUT);
+    pinMode(MOT_L_FWD, OUTPUT);
+    pinMode(MOT_L_REV, OUTPUT);
+    // pinMode(MOT_R_FWD, OUTPUT);
+    // pinMode(MOT_R_REV, OUTPUT);
 
-    pinMode(MOT_FL_ENCA, INPUT_PULLUP);
-    pinMode(MOT_FR_ENCA, INPUT_PULLUP);
-    // pinMode(MOT_BL_ENCA, INPUT_PULLUP);
-    // pinMode(MOT_BR_ENCA, INPUT_PULLUP);
+    pinMode(MOT_L_ENCA, INPUT_PULLUP);  
+    pinMode(MOT_L_ENCB, INPUT_PULLUP);
     
-    pinMode(MOT_FL_ENCB, INPUT_PULLUP);
-    pinMode(MOT_FR_ENCB, INPUT_PULLUP);
-    // pinMode(MOT_BL_ENCB, INPUT_PULLUP);
-    // pinMode(MOT_BR_ENCB, INPUT_PULLUP);
+    // pinMode(MOT_R_ENCA, INPUT_PULLUP);  
+    // pinMode(MOT_R_ENCB, INPUT_PULLUP);
 
-    // Setup timer for motor control
-    attachInterrupt(digitalPinToInterrupt(MOT_FL_ENCA), handle_fl_mot_ticks, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(MOT_FR_ENCA), handle_fr_mot_ticks, CHANGE);
-    // attachInterrupt(digitalPinToInterrupt(MOT_BL_ENCA), handle_bl_mot_ticks, CHANGE);
-    // attachInterrupt(digitalPinToInterrupt(MOT_BR_ENCA), handle_br_mot_ticks, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(MOT_L_ENCA), handle_l_mot_ticks, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(MOT_R_ENCA), handle_r_mot_ticks, CHANGE);
 
-    TCCR1B = (1<<CS10) | (1<<CS12) | (1<<WGM12); // Prescaler 1024 and timer counter reset on compare value
-	TCNT1 = 0; // set initial counter value to 0
-	OCR1A = (16000000/(1024*1))-1; // set compare value to 15624
-	TIMSK1 |= (1<<OCIE1A);
+    attachInterrupt(digitalPinToInterrupt(ACT1_ENCA), handle_act1_ticks, CHANGE);
 
-}
+    TCCR1B = (1<<CS10) | (1<<CS12) | (1<<WGM12);  
+    TCNT1 = 0;
+    OCR1A = (16000000UL / (1024UL * 50UL)) - 1;
+    TIMSK1 |= (1<<OCIE1A);
+
+    collectorServo1.attach(SERVO_1);
+    collectorServo2.attach(SERVO_2);
+
+};
 
 void loop() {
+    // analogWrite(MOT_L_EN,int(output_l));
+    // digitalWrite(MOT_L_REV,0);  
+    // Serial.println(v_l_actual);
+    // Serial.println(int(output_l));
+
+    // Serial.println(mot_ticks[0]);
     switch (currentState)
     {
     case START:
-        break;
+        nextState();
+
     case COLLECT:
-        break;
+        currentCollect = CollectState::ALIGN;
+        switch (currentCollect)
+        {
+            case CollectState::ALIGN:
+                break;
+            case CollectState::ACTUATOR_EXTEND:
+                drive_actuators();
+                break;
+            case CollectState::GRAB:
+                drive_servo();
+                break;
+            case CollectState::ACTUATOR_RETRACT:
+                drive_actuators();
+                break;
+            case CollectState::COLLECT_DONE:
+                nextState();
+        }
     case MOVE:
         break;
     case LIFT:
